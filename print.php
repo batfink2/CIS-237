0e7ab021818e15c3c20953940cd76d4dd3c9c273
batfink2 <61248180+batfink2@users.noreply.github.com>
2020-02-25 10:39:05 -0500

    Add files via upload

---
+++ print.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title>Movie Log</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/Style.css">
+	</head>
+	<body>
+
+			<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+
+				<a class="navbar-brand" href="#">Movie Log</a>
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="Add.html">Add Movie</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="print.php">Movies Watched</a>
+					</li>
+				</ul>
+			</nav>
+
+		<h1>Movie List</h1>
+    </body>
+</html>
+<?php
+?>
