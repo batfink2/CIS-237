6fd053061378033300e5eba984eaf524b1c31228
batfink2 <61248180+batfink2@users.noreply.github.com>
2020-02-28 22:29:07 -0500

    Add files via upload

--- print.php
+++ print.php
@@ -4,11 +4,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<title>Movie Log</title>
+        <! –– stylesheet links-->
         <link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/Style.css">
 	</head>
 	<body>
 
+    <! –– navbar setup-->
 			<nav class="navbar navbar-expand-md bg-dark navbar-dark">
 
 				<a class="navbar-brand" href="#">Movie Log</a>
@@ -22,8 +24,33 @@
 				</ul>
 			</nav>
 
-		<h1>Movie List</h1>
+            <h1>Movie List</h1>
+
+            <?php
+            // open/create the list file
+            $myfile = fopen("list.txt", "a+");
+
+            // if the inputs are set set them to a variable
+            if(isset($_POST['MovieName']))
+            {
+                $movie=$_POST['MovieName'].PHP_EOL;
+                $director=$_POST['DirectorName'].PHP_EOL;
+                $art=$_POST['Artist'].PHP_EOL;
+                $genre=$_POST['Genre'].PHP_EOL;
+                $rate=$_POST['Rating'].PHP_EOL;
+
+                // open the file to write to it new items and current items
+                $fp = fopen('list.txt', 'a');
+
+                fwrite($fp,$movie."".$director."".$art."".$genre."".$rate);
+                fclose($fp);
+            }
+            // print out the file
+            while(!feof($myfile)) {
+                echo fgets($myfile) . "<br>";
+            }
+
+            ?>
+
     </body>
 </html>
-<?php
-?>
